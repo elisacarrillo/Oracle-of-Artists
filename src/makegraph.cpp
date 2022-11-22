0ce35c3427cc795dca63c78bec9844147daa0856
jsanthanam10 <jessica.santhanam@gmail.com>
2022-11-21 19:05:54 -0800

    quote fixing

--- src/makegraph.cpp
+++ src/makegraph.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
 
 MakeGraph::MakeGraph(string filename) {
@@ -68,13 +69,14 @@ MakeGraph::MakeGraph(string filename) {
         }
 
     }
+}
 
-void MakeGraph::BFS_Search(Vertex v1) {
+void MakeGraph::BFS_Search(Vertex v) {
     // Mark all the vertices as not visited
     std::map<Vertex, bool> visited;
     std::vector<Vertex> v = g_.getVertices();
-    for (unsigned int i = 0; i < v.size; i++) {
-        visited.insert(std::pair<Vertex,bool>(v[i], false))
+    for (unsigned int i = 0; i < v.size(); i++) {
+        visited[Vertex] = false;
     }
 
     // Create a queue for BFS
@@ -94,7 +96,7 @@ void MakeGraph::BFS_Search(Vertex v1) {
         // Get all adjacent vertices of the dequeued
         // vertex s. If a adjacent has not been visited,
         // then mark it visited and enqueue it
-        vector<Vertex> adj = v.getAdjacent();
+        vector<Vertex> adj = g_.getAdjacent(v);
         for (Vertex adjacent : adj) {
             if (!visited[adjacent]) {
                 visited[adjacent] = true;
@@ -105,46 +107,42 @@ void MakeGraph::BFS_Search(Vertex v1) {
 }
 
 
-std::vector<Vertex> MakeGraph::BFS_Path(Vertex v1, Vertex v2) {
-    // Mark all the vertices as not visited
-    std::map<Vertex, bool> visited;
-    std::vector<Vertex> v = g_.getVertices();
-    for (unsigned int i = 0; i < v.size; i++) {
-        visited.insert(std::pair<Vertex,bool>(v[i], false))
-    }
+// std::vector<Vertex> MakeGraph::BFS_Path(Vertex v1, Vertex v2) {
+//     // Mark all the vertices as not visited
+//     std::map<Vertex, bool> visited;
+//     std::vector<Vertex> v = g_.getVertices();
+//     for (unsigned int i = 0; i < v.size; i++) {
+//         visited.insert(std::pair<Vertex,bool>(v[i], false))
+//     }
 
-    // Create a queue for BFS
-    std::list<Vertex> queue;
-    std::vector<Vertex> path;
-    // Mark the current node as visited and enqueue it
-    visited[v] = true;
-    queue.push_back(v);
+//     // Create a queue for BFS
+//     std::list<Vertex> queue;
+//     std::vector<Vertex> path;
+//     // Mark the current node as visited and enqueue it
+//     visited[v] = true;
+//     queue.push_back(v);
  
-    while(!queue.empty()) {
-        // Dequeue a vertex from queue and print it
-        v = queue.front();
-        path.push_back(v);
-        //cout << v << " ";
-        queue.pop_front();
+//     while(!queue.empty()) {
+//         // Dequeue a vertex from queue and print it
+//         v = queue.front();
+//         path.push_back(v);
+//         //cout << v << " ";
+//         queue.pop_front();
  
-        // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited,
-        // then mark it visited and enqueue it
-        vector<Vertex> adj = v.getAdjacent();
-        for (Vertex adjacent : adj) {
-            if (!visited[adjacent]) {
-                visited[adjacent] = true;
-                queue.push_back(adjacent);
-            }
-            if (adjacent == v2) {
-                return path;
-            }
-        }
-    }
-    return path;
-       
-
-
-
-
-}
+//         // Get all adjacent vertices of the dequeued
+//         // vertex s. If a adjacent has not been visited,
+//         // then mark it visited and enqueue it
+//         vector<Vertex> adj = v.getAdjacent();
+//         for (Vertex adjacent : adj) {
+//             if (!visited[adjacent]) {
+//                 visited[adjacent] = true;
+//                 queue.push_back(adjacent);
+//             }
+//             if (adjacent == v2) {
+//                 return path;
+//             }
+//         }
+//     }
+//     return path;
+    
+// }
