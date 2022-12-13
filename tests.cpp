6b1d52619bc1de996794d4fc65b789c4ee554c26
pl235521 <56169225+pl235521@users.noreply.github.com>
2022-12-12 21:58:14 -0600

    Jai Final

--- tests.cpp
+++ tests.cpp
@@ -15,9 +15,9 @@ TEST_CASE("BFS") {
     //create solution vector and push in correct output
     //TODO: push back correct solution to sol
     std::vector<std::pair<Vertex, std::string>> sol;
-    sol.push_back();
-    sol.push_back();
-    sol.push_back();
+    sol.push_back(std::pair<Vertex, std::string> ("The Weeknd", "empty"));
+    sol.push_back(std::pair<Vertex, std::string> ("Calvin Harris", "Over Now (with The Weeknd)"));
+    sol.push_back(std::pair<Vertex, std::string> ("Ariana Grande", "Heatstroke (feat. Young Thug Pharrell Williams & Ariana Grande)"));
 
     REQUIRE(sol == output);
     REQUIRE(invalidOutput == std::vector<std::pair<Vertex, std::string>>());
@@ -66,71 +66,56 @@ TEST_CASE("PageRank") {
 
 }
 
-TEST_CASE("BaconNumber") { 
+TEST_CASE("BetweenessCentrality") { 
 
     Vertex artist1 = "The Weeknd";
-    int artist1popularity= g.ArtistsPopularity(artist1);
+    std::map<int, int> artist1popularity= g.ArtistsPopularity(artist1);
     
     Vertex artist2 = "Ariana Grande";
     int artist2popularity= g.ArtistsPopularity(artist2);
 
     //TODO: replace "bob" w correct most popular artist
     Vertex mostPopArtist="Traditional";
-    Vertex mostPopSol=g.MostPopularArtist();
+    std::map<int, int> artist2popularity= g.ArtistsPopularity(artist2);
     
 
     //TODO: Replace -1 w correct popularity
-    REQUIRE(artist1popularity == 75977);
-    REQUIRE(artist1popularity == 5347);
+    REQUIRE(artist1popularity.begin()->first == 0);
+    REQUIRE(artist1popularity.begin()->second == 1);
+    REQUIRE(artist2popularity.begin()->first == 0);
+    REQUIRE(artist2popularity.begin()->second == 1);
 
-    REQUIRE(mostPopArtist == mostPopSol);
+    auto iterator1 = artist1popularity.begin();
+    iterator1++;
+    REQUIRE(iterator1->first == 1);
+    REQUIRE(iterator1->second == 12);
+
+    auto iterator2 = artist2popularity.begin();
+    iterator2++;
+    REQUIRE(iterator2->first == 1);
+    REQUIRE(iterator2->second == 38);
+
+    iterator1++;
+    REQUIRE(iterator1->first == 2);
+    REQUIRE(iterator1->second == 383);
+    iterator2++;
+    REQUIRE(iterator2->first == 2);
+    REQUIRE(iterator2->second == 487);
 
 }
 
 
 TEST_CASE("Connected Components") { 
 
-    //create graph and run pagerank
 
-    Vertex artist1 = "The Weeknd";
-    int artist1popularity= g.ArtistsPopularity(artist1);
     
-    Vertex artist2 = "Ariana Grande";
-    int artist2popularity= g.ArtistsPopularity(artist2);
+    int numComponents= g.countConnectedComponents();
+    
+    int numVertices = g.getGraph().getVertices().size();
 
-    //TODO: replace "bob" w correct most popular artist
-    Vertex mostPopArtist="Traditional";
-    Vertex mostPopSol=g.MostPopularArtist();
     
 
-    //TODO: Replace -1 w correct popularity
-    REQUIRE(artist1popularity == 75977);
-    REQUIRE(artist1popularity == 5347);
-
-    REQUIRE(mostPopArtist == mostPopSol);
-
-}
-
-TEST_CASE("BetweenessCentrality") { 
-
-    //create graph and bacon num alg, saves results to table1, table2, table3
-    MakeGraph g("tracks_twoartists.txt");
-    Vertex artist1 = "The Weeknd";
-    Vertex artist2 = "Ariana Grande";
-    Vertex artist3 = "Idina Menzel";
-    std::map<int, int> table1= g.BaconNumber(artist1);
-    std::map<int, int> table2=g.BaconNumber(artist2);
-    std::map<int, int> table3=g.BaconNumber(artist3);
-    
-
-    //TODO: create solution maps and push in correct output
-    std::map<int, int> sol1;
-    std::map<int, int> sol2;
-    std::map<int, int> sol3;
-    
-
-    REQUIRE(sol1 == table1);
-    REQUIRE(sol2 == table2);
-    REQUIRE(sol3 == table3);
+    REQUIRE(numComponents == 7898);
+    REQUIRE(numVertices == 86051);
 
 }
